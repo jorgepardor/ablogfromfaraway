5763232bd22a0b1cc4bb5b65475a6364358644ec
jmmonzonn <jmmonzonn@gmail.com>
2022-02-22 22:42:13 +0000

    hola que tal

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -10,9 +10,13 @@ export const Home = () => {
 	const starships = 'starships/';
 
 	const arrayPath = [people, planets, starships];
+
+	const randomPath = () => {
+		return Math.floor(Math.random()* 3)
+	}
 	
 	useEffect(() => {
-		fetch(`${url}${arrayPath[index]}`)
+		fetch(`${url}${arrayPath[0]}`)
 			.then((response) => response.json())
 			.then((result) => {
 				console.log("useEffect funcionando");
