224ab9684cb8b34f16c0d388b6434f142f8aebee
jmmonzonn <jmmonzonn@gmail.com>
2022-02-24 20:05:32 +0000

    info character, useParams

--- src/js/component/cardStarships.js
+++ src/js/component/cardStarships.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import PropTypes from 'prop-types';
+import { Link, useParams } from "react-router-dom";
 
 export const CardStarships = ({starship}) => {
     const { store, actions} = useContext(Context);
@@ -14,17 +15,21 @@ export const CardStarships = ({starship}) => {
     const getInfo = async () => {
         let data = await actions.getInfoStarship(starship.uid);
         setInfo(data);
-        console.log(data);
+        // console.log(data);
     }
 
     return (
 
-        <div className="card">
+        <div className="card bg-dark">
             <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">{starship.name}</h5>
+            <div className="card-body text-center">
+                <h5 className="card-title lead text-light">{starship.name}</h5>
                 <p className="card-text">Manufacturer: {info.manufacturer}</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <Link to={`/starship/${starship.uid}`}>
+                    <span className="btn btn-primary btn-lg" href="#" role="button">
+                        More info
+                    </span>
+			    </Link>
             </div>
         </div>
     )
--- src/js/layout.js
+++ src/js/layout.js
@@ -4,7 +4,7 @@ import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import CharacterInfo from "./views/characterinfo.js";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -28,11 +28,14 @@ const Layout = () => {
 						<Route exact path="/demo">
 							<Demo />
 						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
+						<Route exact path="/character/:theid">
+							<CharacterInfo />
 						</Route>
-						<Route exact path="character/:uid">
-							<Single />
+						<Route exact path="/planet/:theid">
+							<CharacterInfo />
+						</Route>
+						<Route exact path="/starship/:theid">
+							<CharacterInfo />
 						</Route>
 						<Route>
 							<h1>Not found!</h1>
