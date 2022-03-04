4bd7cddcc5ebd683344cd859214c62942ae0e9cf
jorgepardor <jorgeluis.mail@gmail.com>
2022-03-04 08:48:29 +0000

    mas cambios en las cards -wip-

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,7 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			chars: [],
+			chars: [{"uid":"1","name":"Luke Skywalker","url":"https://www.swapi.tech/api/people/1"},{"uid":"2","name":"C-3PO","url":"https://www.swapi.tech/api/people/2"},{"uid":"3","name":"R2-D2","url":"https://www.swapi.tech/api/people/3"},{"uid":"4","name":"Darth Vader","url":"https://www.swapi.tech/api/people/4"},{"uid":"5","name":"Leia Organa","url":"https://www.swapi.tech/api/people/5"},{"uid":"6","name":"Owen Lars","url":"https://www.swapi.tech/api/people/6"},{"uid":"7","name":"Beru Whitesun lars","url":"https://www.swapi.tech/api/people/7"},{"uid":"8","name":"R5-D4","url":"https://www.swapi.tech/api/people/8"},{"uid":"9","name":"Biggs Darklighter","url":"https://www.swapi.tech/api/people/9"},{"uid":"10","name":"Obi-Wan Kenobi","url":"https://www.swapi.tech/api/people/10"}],
 			planets: [],
 			starships: [],
 			charUrl: "",
@@ -9,6 +9,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			shipUrl: "",
 			favourites: []
 		},
+
 		actions: {
 			getCharacters: () => {
 				if (getStore().charUrl == "") {
@@ -22,26 +23,46 @@ const getState = ({ getStore, getActions, setStore }) => {
 					.then((data) => setStore({chars: data.results, charUrl: data.next}));
 				}
 			},
+
 			getPlanets: () => {
-				fetch('https://www.swapi.tech/api/planets?page=1')
-				.then((response) => response.json())
-				.then((data) => setStore({planets: data.results, planUrl: data.next}));
+					if (getStore().planUrl == "") {
+					fetch('https://www.swapi.tech/api/planets?page=1')
+					.then((response) => response.json())
+					.then((data) => setStore({planets: data.results, planUrl: data.next}));
+				}
+					else if (getStore().planUrl != "") {
+					fetch(getStore().planUrl)
+					.then((response) => response.json())
+					.then((data) => setStore({planets: data.results, planUrl: data.next}));
+				}
 			},
+
 			getStarships: () => {
+				if (getStore().shipUrl == "") {
 				fetch('https://www.swapi.tech/api/starships?page=1')
 				.then((response) => response.json())
 				.then((data) => setStore({starships: data.results, shipUrl: data.next}));
+				}
+				else if (getStore().shipUrl != "") {
+					fetch(getStore().shipUrl)
+					.then((response) => response.json())
+					.then((data) => setStore({starships: data.results, shipUrl: data.next}));
+				}
+
 			},
+
 			getInfoCharacter: async (id) => {
 				let response = await fetch('https://www.swapi.tech/api/people/'+ id)
 				let data = await response.json();
 				return data.result.properties;
 			},
+
 			getInfoPlanet: async (id) => {
 				let response = await fetch('https://www.swapi.tech/api/planets/'+ id)
 				let data = await response.json();
 				return data.result.properties;
 			},
+			
 			getInfoStarship: async (id) => {
 				let response = await fetch('https://www.swapi.tech/api/starships/'+ id)
 				let data = await response.json();
